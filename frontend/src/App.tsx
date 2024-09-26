d546977e85bfd2a7aad35680563b1d00447f9ad9
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-09-26 15:49:46 +0530

    feat-frontend: setup shadcn ui lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Dalpat</h1>
+      <Button>Click me</Button>
     </>
   );
 }
